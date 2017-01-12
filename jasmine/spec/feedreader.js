bfb19ff51c14f4c54c97a6044816cabc106623e2
Waseem Ahmed <alphawaseem@gmail.com>
2017-01-12 21:05:44 +0530

    test : name field is defined and not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,10 +44,19 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('name field is defined and not empty', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+
+    });
 
     /* TODO: Write a test that ensures the menu element is
      * hidden by default. You'll have to analyze the HTML and
